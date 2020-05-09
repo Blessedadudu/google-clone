b4c3aa6526da5e6467e0c2eefe2bf0d6a6b2058e
Blessedadudu <adududuke@gmail.com>
2020-05-09 13:03:37 +0100

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header'
 
 function App() {
   return ( 
     <div>
      <Navbar/>
-     <h1>Hello</h1>
+     <Header/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './Navbar.scss'
+import './Navbar.scss';
+import gmailLogo from '../../assets/logo-gmail.png';
 
 const Navbar = () => {
     return (
         <nav className='nav'>
-            <h1>Navbar Component</h1>
+            <div className='nav--body'>
+                <div>
+                    <p>Gmail</p>
+                </div>
+                <img src={gmailLogo} alt='the gmail logo' />
+                <h1>Navbar Component</h1>
+            </div>
         </nav>
     )
 }
