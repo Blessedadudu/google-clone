60f9faec12aae7782221b60997a517898b90bb63
Blessedadudu <adududuke@gmail.com>
2020-05-09 12:33:42 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  return ( 
+    <div>
+     <Navbar/>
+     <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.scss'
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <h1>Navbar Component</h1>
+        </nav>
+    )
+}
+
+export default Navbar
