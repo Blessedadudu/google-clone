33d0d9e3953ed4cc373886559d1af3c6f1c62d32
Blessedadudu <adududuke@gmail.com>
2020-05-14 22:57:49 +0100

    update

--- src/components/CarouselHeader/CarouselHeader.js
+++ src/components/CarouselHeader/CarouselHeader.js
@@ -5,20 +5,29 @@ import './CarouselHeader.scss'
 const CarouselHeader = () => {
     let carouselInfo = [ 1, 2,3,4,5 ];
 
-    const [x, setX] = useState(0)
+    const [x, setX] = useState(0);
+    
+    let goLeft = () => {
+        setX( x + 100 )
+    }
+
+    let goRight = () => {
+        setX( x - 100 )
+    }
+
     return (
         <div className='slider'>
             { 
                 carouselInfo.map((items, id) => {
                   return (
-                    <div key={id} className='slide'  style={{ transform: `transform(${x}%)`}}>
+                    <div key={id} className='slide' style={{ transform: `translateX(${x}%)`}}>
                         {items} 
                     </div>
                   ) 
                 })
             }
-            <button id='goLeft'>Left</button>
-            <button id='goRight'>Right</button>
+            <button id='goLeft' onClick={goLeft}>Left</button>
+            <button id='goRight' onClick={goRight}>Right</button>
         </div>
     )
 }
