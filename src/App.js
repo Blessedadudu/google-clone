c21f4fe23721e4e00ee13681558a3468cc082a34
Blessedadudu <adududuke@gmail.com>
2020-05-16 09:12:09 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import CarouselHeader from './components/CarouselHeader/CarouselHeader';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return ( 
--- src/components/CarouselHeader/CarouselHeader.js
+++ src/components/CarouselHeader/CarouselHeader.js
@@ -3,7 +3,6 @@ import './CarouselHeader.scss';
 import carousel1 from '../../assets/carousel1.png';
 import carousel2 from '../../assets/carousel2.png';
 import carousel3 from '../../assets/carousel3.png';
-import { Carousel } from 'react-bootstrap';
 
 
 
@@ -37,46 +36,7 @@ const CarouselHeader = () => {
 
     return (
         <div>
-        <Carousel>
-            <Carousel.Item>
-                <img
-                className="d-block w-10 carouselImg1"
-                src={carouselInfo[0].image}
-                alt="First slide"
-                />
-                <Carousel.Caption>
-                <h3>{carouselInfo[0].heading}</h3>
-                <h1>{carouselInfo[0].heading}</h1>
-                <h6>{carouselInfo[0].little}</h6>
-                <button>{carouselInfo[0].button}</button>
-                {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            </Carousel>
+           
         </div>
     )
         
