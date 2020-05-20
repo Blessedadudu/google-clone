a5983c0c7224dca1157109a7958d869808a1ad38
Blessedadudu <adududuke@gmail.com>
2020-05-20 11:56:14 +0100

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import './Footer.scss'
+import './Footer.scss';
+import circlehelp from '../../assets/SVG/help-with-circle.svg'
 
 class Footer extends Component {
     render() {
         return (
-            <Footer className='footer'>
+            <footer className='footer'>
                     <h2>Google</h2>
-                    <div>
+                    <div className='footFlex'>
                         <p>Privacy </p>
                         <p> Terms</p>
                         <p>About Google</p>
                         <p> Google Products</p>
                         <p> Our policy</p>
                     </div>
-                    <div>
-                        <p>logo</p>
-                        <p>Help</p>
+                    <div className='footFlex2'>
+                        <a href="https://support.google.com/mail/?hl=en#topic=7065107" target="_blank" rel="noopener noreferrer">
+                            <img src={circlehelp} alt='svg' />
+                            <p>Help</p>
+                        </a>
                         <p>Select Language</p>
 
                     </div>
-            </Footer>
+            </footer>
         )
     }
 }
