b1a668f4711dbac6b0303c57e778698814fe7d07
Blessedadudu <adududuke@gmail.com>
2020-05-20 14:06:36 +0100

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
         return (
             <footer className='footer'>
                     <h2>Google</h2>
-                    <div className='footFlex'>
+                    <div>
                         <p>Privacy </p>
                         <p> Terms</p>
                         <p>About Google</p>
@@ -15,11 +15,19 @@ class Footer extends Component {
                         <p> Our policy</p>
                     </div>
                     <div className='footFlex2'>
-                        <a href="https://support.google.com/mail/?hl=en#topic=7065107" target="_blank" rel="noopener noreferrer">
+                        <a href="https://support.google.com/mail/?hl=en#topic=7065107" target="_blank" rel="noopener noreferrer" className='footLink'>
                             <img src={circlehelp} alt='svg' />
                             <p>Help</p>
                         </a>
-                        <p>Select Language</p>
+                        <div className='footSelect'>
+                        {/* <label for="cars">Choose a car:</label> */}
+                            <select className="select-css">
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                            </select>
+                        </div>
 
                     </div>
             </footer>
